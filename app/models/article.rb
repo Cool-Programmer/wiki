1180e3a2a953f3fd7ff0defdb4b86cd7708d7e6a
= <margaryan.mher.28@gmail.com>
2016-11-29 09:41:08 +0400

    Validation, user auth

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true,
+            length: {
+              minimum: 5,
+              maximum: 255
+            }
+  validates :content, presence: true,
+            length: {
+              minimum: 20
+            }
 end
